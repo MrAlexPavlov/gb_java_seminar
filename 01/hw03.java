99fd58f67f3bc3629a924eb0d62b9bd8c395019f
APavlov <markmarkovkin@gmail.com>
2023-03-04 13:38:51 +0300

    resolved home work task03

---
+++ 01/hw03.java
@@ -0,0 +1,55 @@
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+/**
+ * Реализовать простой калькулятор (+ - / *)
+ * Ввод числа ->
+ * Ввод знака ->
+ * Ввод числа ->
+ */
+public class hw03 {
+
+    public static void main(String[] args) {
+        float a = Float.parseFloat( input("Ввод числа `a`: ") );
+        Character what = input("Ввод знака: ").charAt(0);
+        float b = Float.parseFloat( input("Ввод числа `b`: ") );
+
+        calc(a, b, what);
+    }
+
+    public static void calc(float a, float b, Character what){
+        float res = 0;
+        if ( what == '+' ){
+            res = (float) a + b;
+        } else if (what == '-') {
+            res = (float) a - b;
+        } else if (what == '*') {
+            res = (float) a * b;
+        } else if (what == '/') {
+            if (b != 0) {
+                res = (float) a / b;
+            } else {
+                System.out.println("Делить на ноль нельзя!");
+                return;
+            }
+        } else {
+            System.out.printf("Действие `%c` не найдено!", what);
+            return;
+        }
+        
+        NumberFormat nf = new DecimalFormat("#.######");
+        System.out.println( nf.format(a) + what + nf.format(b) + " = "+ nf.format(res) );
+    
+        return;
+    }
+
+    public static String input(String text) {
+        Scanner iScan = new Scanner(System.in);
+        System.out.println(text);
+        String result = iScan.nextLine();
+        return result;
+    }
+}
