627b3843a8d0cc6aadf8d6a63a17832d35b3d3c2
APavlov <markmarkovkin@gmail.com>
2023-03-05 20:55:22 +0300

    correct resolved home work task 04

--- 01/hw04.java
+++ 01/hw04.java
@@ -12,50 +12,34 @@ import java.util.Scanner;
 public class hw04 {
     public static void main(String[] args) {
         String users_func = input("Задайте уровнение :");
-        // users_func = "2? + ?5 = 69";
         String[] arrStr = users_func.split("\s");
-        // System.out.println(arrStr[0] + ' ' + arrStr[2] + " = " + arrStr[4]);
 
-        int q = whatIsit(arrStr[0]);
-        int w = whatIsit(arrStr[2]);
-        int e = whatIsit(arrStr[4]);
+        String q = arrStr[0];
+        String w = arrStr[2];
+        int e = Integer.parseInt(arrStr[4]);
 
-        int anq = 0;
-        int anw = 0;
-        int answer_q = 0;
-        int answer_w = 0;
-        int whereQ = 0;
-        
-        Integer temp = e - (q + w);
-        // System.out.println("temp = "+temp);
-        if (temp<10){
-            anq = temp / 2;
-            anw = anq;
-        } else{
-            // На какой позиции вопрос в первом числе
-            whereQ = whereIs(arrStr[0]);
-            // На что надо заменить этот вопрос берем из временной переменной
-            Character r = temp.toString().charAt(whereQ);
-            anq = Character.getNumericValue(r);
+        int try_q = 0;
+        int try_w = 0;
+        boolean answer = false;
+        for (int i = 0; i < 10; i++){
+            try_q = replace(q, i);
 
-            // На какой позиции вопрос во втором числе
-            whereQ = whereIs(arrStr[0]);
-            // На что надо заменить этот вопрос берем из временной переменной
-            r = temp.toString().charAt(whereQ);
-            anw = Character.getNumericValue(r);
+            for (int j = 0; j < 10; j++){
+                try_w = replace(w, j);
+                if (try_q + try_w == e){
+                    answer = true; 
+                    break;
+                }
+            }
+        if (answer) break;
         }
 
-        answer_q = replace(arrStr[0], anq);
-        answer_w = replace(arrStr[2], anw);
-
-        if ((answer_q + answer_w) != e) {
-            System.out.println("Решений не имеет!");
+        if (answer){
+            System.out.println(try_q + " + " + try_w + " = " + e);
         } else {
-            System.out.println(answer_q + " + " + answer_w + " = " + e);
+            System.out.println("Решений не имеет!");
         }
-
-        
-
+ 
     }
 
     public static int replace(String msg, int a){
@@ -68,31 +52,6 @@ public class hw04 {
             }
         }
         return Integer.parseInt(temp);
-    }
-
-    public static int whereIs(String msg){
-        int res = 0;
-        for (int i = 0; i < msg.length(); i++) {
-            if (msg.charAt(i) == '?') {
-                res = i;
-            }
-        }
-        return res;
-    }
-
-    public static int whatIsit(String msg) {
-        int res = 0;
-        int a = 0;
-        double prod = Math.pow(10, msg.length());
-        for (int i = 0; i < msg.length(); i++) {
-
-            if (msg.charAt(i) != '?') {
-                a = Character.getNumericValue(msg.charAt(i));
-                res += a * (prod / 10);
-            }
-            prod = prod / 10;
-        }
-        return res;
     }
 
     public static String input(String text) {
