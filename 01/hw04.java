a106501cfa89805d48018091d343f89552eac9ea
APavlov <markmarkovkin@gmail.com>
2023-03-06 12:50:30 +0300

    small fix

--- 01/hw04.java
+++ 01/hw04.java
@@ -8,47 +8,50 @@ import java.util.Scanner;
  * Предложить хотя бы одно решение или сообщить, что его нет.
  * Ввод: 2? + ?5 = 69
  * Вывод: 24 + 45 = 69
+ * reg ([0-9?]*)[\s+]*([0-9?]*)[\s=]*([0-9]*)
  */
 public class hw04 {
     public static void main(String[] args) {
         String users_func = input("Задайте уровнение :");
         String[] arrStr = users_func.split("\s");
-
+        // Разбиваем уровнение на части по пробелу
         String q = arrStr[0];
+        Character sign = arrStr[1].toCharArray()[0];
         String w = arrStr[2];
         int e = Integer.parseInt(arrStr[4]);
 
         int try_q = 0;
         int try_w = 0;
         boolean answer = false;
-        for (int i = 0; i < 10; i++){
-            try_q = replace(q, i);
 
-            for (int j = 0; j < 10; j++){
+        for (int i = 0; i < 10; i++) {
+            try_q = replace(q, i);
+            for (int j = 0; j < 10; j++) {
                 try_w = replace(w, j);
-                if (try_q + try_w == e){
-                    answer = true; 
-                    break;
-                }
+                if (sign == '+') answer = sum(try_q, try_w, e);
+                if (sign == '-') answer = minus(try_q, try_w, e);
+                if (sign == '*') answer = prod(try_q, try_w, e);
+                if (sign == '/') answer = div(try_q, try_w, e);
+                if (answer) break;
             }
-        if (answer) break;
+            if (answer) break;
         }
 
-        if (answer){
-            System.out.println(try_q + " + " + try_w + " = " + e);
+        if (answer) {
+            System.out.printf("%d %c %d = %d", try_q, sign, try_w, e);
         } else {
             System.out.println("Решений не имеет!");
         }
- 
+
     }
 
-    public static int replace(String msg, int a){
+    public static int replace(String msg, int a) {
         String temp = "";
         for (int i = 0; i < msg.length(); i++) {
             if (msg.charAt(i) == '?') {
                 temp += a;
             } else {
-                temp +=msg.charAt(i);
+                temp += msg.charAt(i);
             }
         }
         return Integer.parseInt(temp);
@@ -60,5 +63,21 @@ public class hw04 {
         String result = iScan.nextLine();
         iScan.close();
         return result;
+    }
+
+    public static boolean sum(int a, int b, int answer) {
+        return a + b == answer ? true : false;
+    }
+
+    public static boolean minus(int a, int b, int answer) {
+        return a - b == answer ? true : false;
+    }
+
+    public static boolean prod(int a, int b, int answer) {
+        return a * b == answer ? true : false;
+    }
+
+    public static boolean div(int a, int b, int answer) {
+        return a / b == answer ? true : false;
     }
 }
