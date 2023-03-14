b151f22d60faa8a11702bef1df5015d0abcefe6d
APavlov <markmarkovkin@gmail.com>
2023-03-14 10:38:26 +0300

    bug fix

--- 04/task01.java
+++ 04/task01.java
@@ -1,13 +1,14 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * 1) Замерьте время, за которое в ArrayList добавятся 10000 элементов.
  * 2) Замерьте время, за которое в LinkedList добавятся 10000 элементов. Сравните с предыдущим.
  */
 public class task01 {
-    public static int cnt = 1000_000_000;
+    public static int cnt = 10_000_000;
     public static void main(String[] args) {
-        
+
         long time = System.currentTimeMillis();
         createArrayList(cnt);
         System.out.println(System.currentTimeMillis() - time);
@@ -19,15 +20,15 @@ public class task01 {
 
     public static void createArrayList(int cnt) {
         ArrayList<String> res_arr = new ArrayList<String>(cnt);
-        for (int i = 0; i <res_arr.size(); i++) {
-            res_arr.add(0,"+");
+        for (int i = 0; i <cnt; i++) {
+            res_arr.add("+");
         }
     }
 
     public static void createLinkedList(int cnt) {
-        ArrayList<String> res_arr = new ArrayList<String>(cnt);
-        for (int i = 0; i <res_arr.size(); i++) {
-            res_arr.add(0,"-");
+        LinkedList<String> res_arr = new LinkedList<String>();
+        for (int i = 0; i <cnt; i++) {
+            res_arr.add("-");
         }
     }
     
